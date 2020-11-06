bc0872ee494b5765a418b60fae3c14e57d8d7e35
madiwa Simon <mad.simon01@gmail.com>
2020-11-06 15:46:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -43,6 +43,6 @@ end
 
 def update_element_from_index(array, index_number, element)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array[4] = "totally"
-  p array
+  array[3] = "totally"
+  p array[3]
 end
